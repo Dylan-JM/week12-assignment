1015de10b9a2cc316d31619ca44ddef16b625b61
Dylan Marley <dylanjmarley01@gmail.com>
2026-02-13 11:58:03 +0000

    readd files

---
+++ src/app/(routes)/contact/page.js
@@ -0,0 +1,18 @@
+// import Header from '@/components/Header'
+// import Footer from '@/components/Footer'
+import './contact.css'
+
+export default function ContactPage(){
+	return(
+		<>
+			<div>
+                {/* <Header /> */}
+                {/* <Nav /> */}
+            </div>
+            <section className='contact-form'>
+                {/* <ContactForm /> */}
+            </section>
+			{/* <Footer /> */}
+		</>
+	);
+}
