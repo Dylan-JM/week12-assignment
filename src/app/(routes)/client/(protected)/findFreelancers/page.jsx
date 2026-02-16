285be2ed81e56e481f08493d952fcf843e572c89
Adam Booth <adam_booth@live.co.uk>
2026-02-16 11:05:09 +0000

    find freelancer page filters added

--- src/app/(routes)/client/(protected)/findFreelancers/page.jsx
+++ src/app/(routes)/client/(protected)/findFreelancers/page.jsx
@@ -1,6 +1,7 @@
 import { db } from "@/lib/dbConnection";
+import FreelancersList from "@/components/FreelancersList";
 
-export default async function clientFindFreelancersPage() {
+export default async function ClientFindFreelancersPage() {
   const { rows } = await db.query(`SELECT * FROM fm_freelancers`);
 
   const freelancers = rows.map((freelancer) => ({
@@ -11,29 +12,5 @@ export default async function clientFindFreelancersPage() {
         : (freelancer.skills ?? []),
   }));
 
-  return (
-    <div className="all-client-jobs-container">
-      {freelancers.map((freelancer) => (
-        <div className="client-job-container" key={freelancer.id}>
-          <h1 className="job-title">Username : {freelancer.name}</h1>
-          <p className="profile-bio">Bio : {freelancer.bio}</p>
-
-          <div className="specific-jobs-details">
-            <div>
-              <h2 className="profile-skills">
-                <strong>Skills:</strong>
-              </h2>
-              <ul className="profile-skills">
-                {freelancer.skills.map((skill, index) => (
-                  <li className="job-skill" key={index}>
-                    {skill}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <FreelancersList freelancers={freelancers} />;
 }
