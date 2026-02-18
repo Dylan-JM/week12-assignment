af0907fc338474ea86187f1225a6a49cb8051c61
Adam Booth <adam_booth@live.co.uk>
2026-02-18 13:34:10 +0000

    added job details and css

--- src/app/(routes)/freelancer/(protected)/jobs/[id]/page.jsx
+++ src/app/(routes)/freelancer/(protected)/jobs/[id]/page.jsx
@@ -1,32 +1,43 @@
 import JobsClient from "@/components/JobsClient";
-import {db} from '@/lib/dbConnection'
+import { db } from "@/lib/dbConnection";
 
 export default async function JobsId({ params }) {
-    const {id} = await params;
-    let {rows: incomeRows} = await db.query(`SELECT * FROM fm_income WHERE job_id = $1`,[
-      id
-    ]);
-    // console.log({id});
-    // console.log({incomeRows});
-    const income = [];
-    for (let i=0;i<incomeRows.length;i++){
-        income.push(incomeRows[i].amount);
-    }
-    // console.log(income);
+  const { id } = await params;
+  let { rows: incomeRows } = await db.query(
+    `SELECT * FROM fm_income WHERE job_id = $1`,
+    [id],
+  );
+  // console.log({id});
+  // console.log({incomeRows});
+  const income = [];
+  for (let i = 0; i < incomeRows.length; i++) {
+    income.push(incomeRows[i].amount);
+  }
+  // console.log(income);
 
-    let {rows: expenseRows} = await db.query(`SELECT * FROM fm_expenses WHERE job_id = $1`,[
-      id
-    ]);
-    // console.log({id});
-    // console.log({expenseRows});
-    const expenses = [];
-    for (let i=0;i<expenseRows.length;i++){
-        expenses.push(expenseRows[i].price);
-    }
-    // console.log(expenses);
+  let { rows: expenseRows } = await db.query(
+    `SELECT * FROM fm_expenses WHERE job_id = $1`,
+    [id],
+  );
+  // console.log({id});
+  // console.log({expenseRows});
+  const expenses = [];
+  for (let i = 0; i < expenseRows.length; i++) {
+    expenses.push(expenseRows[i].price);
+  }
+  // console.log(expenses);
 
-    return (
-      <JobsClient id = {id} income={income} expenses={expenses} /> 
-    )
+  let { rows: jobDetails } = await db.query(
+    `SELECT * FROM fm_jobs WHERE id = $1`,
+    [id],
+  );
 
+  return (
+    <JobsClient
+      id={id}
+      income={income}
+      expenses={expenses}
+      jobDetails={jobDetails}
+    />
+  );
 }
--- src/components/JobsClient.jsx
+++ src/components/JobsClient.jsx
@@ -1,37 +1,53 @@
-"use client"
+"use client";
 
-import JobsChart from './JobsChart'
-import Link from 'next/link'
+import JobsChart from "./JobsChart";
+import Link from "next/link";
 
-export default function JobsClient({id, income, expenses}){
-
-    return(
-        <div className='flex flex-col gap-2'>
-            <section className='flex flex-col feature-card-container'>
-                <h2 className='text-2xl'>Job Expenses and Income</h2>
-                <p className='feature-card'>Job details...</p>
-            </section>
-            <section className='flex flex-row'>
-                <section className='flex flex-col'>
-                    <h3 className='text-lg'>Expenses</h3>
-                    <Link href={`/freelancer/jobs/${id}/expense-form`}>Add expense</Link>
-                    <section className='flex flex-col feature-card-container'>
-                        {expenses.map((expense, index)=>{
-                            return <p key={index} className='feature-card'>{expense}</p>
-                        })}
-                    </section>
-                </section>
-                <section className='flex flex-col'>
-                    <h3 className='text-lg'>Income</h3>
-                    <Link href={`/freelancer/jobs/${id}/income-form`}>Add Income</Link>
-                    <section className='feature-card-container'>
-                        {income.map((amount, index)=>{
-                            return <p key={index} className='feature-card'>{amount}</p>
-                        })}
-                    </section>
-                    <JobsChart id={id} income={income} expenses={expenses}/>
-                </section>
-            </section>
-        </div>
-    )
+export default function JobsClient({ id, income, expenses, jobDetails }) {
+  console.log(jobDetails);
+  return (
+    <div className="flex flex-col gap-2 analytics-contents">
+      <section className="flex flex-col feature-card-container">
+        <h2 className="text-2xl">
+          <strong>Job Expenses and Income</strong>
+        </h2>
+        <h1>
+          <strong>{jobDetails[0].title}</strong>
+        </h1>
+        <p className="feature-card">
+          <strong>Description : </strong>
+          {jobDetails[0].description}
+        </p>
+      </section>
+      <section className="flex flex-row">
+        <section className="flex flex-col">
+          <h3 className="text-lg">Expenses</h3>
+          <Link href={`/freelancer/jobs/${id}/expense-form`}>Add expense</Link>
+          <section className="flex flex-col feature-card-container">
+            {expenses.map((expense, index) => {
+              return (
+                <p key={index} className="feature-card">
+                  £{expense}
+                </p>
+              );
+            })}
+          </section>
+        </section>
+        <section className="flex flex-col">
+          <h3 className="text-lg">Income</h3>
+          <Link href={`/freelancer/jobs/${id}/income-form`}>Add Income</Link>
+          <section className="feature-card-container">
+            {income.map((amount, index) => {
+              return (
+                <p key={index} className="feature-card">
+                  £{amount}
+                </p>
+              );
+            })}
+          </section>
+          <JobsChart id={id} income={income} expenses={expenses} />
+        </section>
+      </section>
+    </div>
+  );
 }
