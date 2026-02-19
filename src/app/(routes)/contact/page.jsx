8e0132a0eb1c53388e33cf13a77e3958f6526716
Dylan Marley <dylanjmarley01@gmail.com>
2026-02-19 09:17:13 +0000

    style: contact form

--- src/app/(routes)/contact/page.jsx
+++ src/app/(routes)/contact/page.jsx
@@ -14,12 +14,14 @@ export default function Contact() {
           src="https://media.istockphoto.com/id/1412531992/vector/contact-us-banner-speech-bubble-label-sticker-ribbon-template-vector-stock-illustration.jpg?s=612x612&w=0&k=20&c=YYZr8DA0KbCiTTGr43HDWNqVGnhTuLAr8J3MFDncsRE="
           className="contact-us-img"
         />
-        <p className="feature-card">
-          Contact us here. Please give us your purpose and details and we will
-          get back to you soon. Alternatvely please use our chat messenging
-          service.
-        </p>
-        <ContactForm />
+        <div className="contact-cards-wrapper">
+          <p className="feature-card">
+            Contact us here. Please give us your purpose and details and we will
+            get back to you soon. Alternatvely please use our chat messenging
+            service.
+          </p>
+          <ContactForm />
+        </div>
       </section>
     </>
   );
